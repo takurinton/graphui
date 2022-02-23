f33dac53bb99a2d7dd10a2f238d65f342783429e
takurinton <takurinton@takurinton.com>
2022-02-23 18:40:10 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/*
+  メモ: 
+    - 将来的には切り離すので、ここでは呼ぶだけ
+    - react, react-dom, urql, chakra-ui は devDependencies に入れる
+*/
+
+function App() {
+  return (
+    <>hoge</>
+  )
+}
+
+export default App;
