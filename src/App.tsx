ebc24ede819084c9852070737309036790e2127c
takurinton <takurinton@takurinton.com>
2022-02-26 14:18:41 +0900

    add: select

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,41 @@
     - react, react-dom, urql, chakra-ui は devDependencies に入れる
 */
 
+import { ChakraProvider } from '@chakra-ui/react';
 import { GraphUI } from './graphui/GraphUI';
+
+const schema = `
+type User {
+    id: ID!
+    username: String!
+    email: String!
+    role: Role!
+}
+
+type Hoge {
+    a: Int!
+    b: String!
+    c: Role
+}
+
+type Query {
+    me: User!
+    user(id: ID!): User
+    allUsers: [User]
+    hoge: Hoge
+}
+
+enum Role {
+    USER
+    ADMIN
+}
+`;
 
 function App() {
   return (
-    <GraphUI />
+    <ChakraProvider>
+      <GraphUI schema={schema} />
+    </ChakraProvider>
   )
 }
 
--- src/graphui/GraphUI.tsx
+++ src/graphui/GraphUI.tsx
@@ -1,57 +1,24 @@
-import { useEffect, useState } from "react";
-import { Flex } from '@chakra-ui/react';
+import React, { useCallback, useEffect, useState } from "react";
+import { chakra } from "@chakra-ui/system";
 import {
-    ASTNode,
+    Flex,
+    Input,
+    Box,
+    Code
+} from '@chakra-ui/react';
+import {
     buildSchema,
     DocumentNode,
     GraphQLField,
     GraphQLNamedType,
-    GraphQLSchema,
-    parse,
     print,
+    parse,
+    ASTNode,
+    SelectionSetNode,
+    GraphQLSchema,
 } from "graphql";
 import { Provider } from './context';
-
-
-const initialQuery = `
-query findUser($userId: ID!) {
-    user(id: $userId) {
-      ...UserFields
-    }
-  }
-  
-  fragment UserFields on User {
-    id
-    username
-  }
-`;
-
-const schema = `
-type User {
-    id: ID!
-    username: String!
-    email: String!
-    role: Role!
-}
-
-type Hoge {
-    a: Int!
-    b: String!
-    c: Role
-}
-
-type Query {
-    me: User!
-    user(id: ID!): User
-    allUsers: [User]
-    hoge: Hoge
-}
-
-enum Role {
-    USER
-    ADMIN
-}
-`;
+import Select from 'react-select';
 
 // inspired by https://github.com/timqian/gql-generator/blob/20f76a0f37f31f961f8d5599cc115748c89fb614/index.js#L39-L55
 const getFieldArgsDict = ({
@@ -111,7 +78,7 @@ const generateGraphQLQuery = ({
         // 例えばネストしてる場合、これは再帰的に generateQuery 関数を呼ぶ必要がある
         childQuery = Object.keys(fields)
             .map(field => {
-                return field;
+                return `${'    '.repeat(currentDepth + 2)}${field}`;
             }).join('\n');
     }
 
@@ -155,17 +122,198 @@ const getQuerysMap = ({
     return res;
 }
 
-export const GraphUI = () => {
-    // set current query string
-    const [query, setQuery] = useState('')
-    // query to astnode
-    const [ast, setAst] = useState<DocumentNode>(parse(initialQuery));
+const getFields = ({
+    schema,
+    name
+}: {
+    schema: string;
+    name: string;
+}) => {
+    const geaphqlSchema = buildSchema(schema);
+    const curerentQueryField = geaphqlSchema.toConfig().query?.getFields()[name] as GraphQLField<any, any, any>;
+    const currentQueryTypeName = curerentQueryField.type.toString().replace(/[[\]!]/g, '');
+    const currentQureyType = geaphqlSchema.getType(currentQueryTypeName) as GraphQLNamedType;
 
+    // @ts-ignore
+    return Object.keys(currentQureyType.getFields()).map(t => ({ value: t, label: t }));
+}
+
+// 以下は query の AST を触る
+
+// query から field を取得する
+const getFieldByQuery = (node: ASTNode): ASTNode => {
+    if (node.kind === 'Document') {
+        return getFieldByQuery(node.definitions[0]);
+    }
+
+    if (node.kind === 'OperationDefinition') {
+        return getFieldByQuery(node.selectionSet);
+    }
+
+    if (node.kind === 'SelectionSet') {
+        return getFieldByQuery(node.selections);
+    }
+
+    if (node.kind === 'Field') {
+        if (node.selectionSet === undefined) {
+            return node.name;
+        }
+        return getFieldByQuery(node.selectionSet);
+    }
+
+    return {} as ASTNode;
+}
+
+const Indent = ({ children }: { children: React.ReactNode }) => {
+    return <Box pl={4}>{children}</Box>;
+}
+
+const RootRenderer = ({ node }: { node: ASTNode }) => {
+    if (node.kind === 'Document') {
+        return (
+            <Box>
+                {node.definitions.map((def, index) => {
+                    return <RootRenderer key={index} node={def} />;
+                })}
+            </Box>
+        );
+    }
+
+    if (node.kind === 'OperationDefinition') {
+        return (
+            <Box border="1px solid white" boxSizing="border-box">
+                <RootRenderer node={node.selectionSet} />
+            </Box>
+        );
+    }
+    if (node.kind === 'SelectionSet') {
+        return (
+            <Box>
+                <Indent>
+                    {node.selections.map((selection, index) => {
+                        return (
+                            <Box key={index}>
+                                <RootRenderer node={selection} />
+                            </Box>
+                        );
+                    })}
+                </Indent>
+            </Box>
+        );
+    }
+
+    if (node.kind === 'Field') {
+        return (
+            <Box d="inline">
+                {node.alias && (
+                    <>
+                        <RootRenderer node={node.alias} />
+                        {`: `}
+                    </>
+                )}
+                <RootRenderer node={node.name} />
+                {'('}
+                {node.arguments && (
+                    <Indent>
+                        {node.arguments.map((arg, idx) => {
+                            return <RootRenderer key={idx} node={arg} />;
+                        })}
+                    </Indent>
+                )}
+                {')'}
+                {node.directives?.map((t, idx) => {
+                    return <RootRenderer key={idx} node={t} />;
+                })}
+            </Box>
+        );
+    }
+    if (node.kind === 'Argument') {
+        return (
+            <Box>
+                <RootRenderer node={node.name} />
+                {': '}
+                <RootRenderer node={node.value} />
+            </Box>
+        );
+    }
+    // Literal
+    if (node.kind === 'Name') {
+        return (
+            <Box d="inline">
+                <Code>{node.value}</Code>
+            </Box>
+        );
+    }
+
+    /** --- Literal --- */
+    if (node.kind === 'StringValue') {
+        return (
+            <Box d="inline">
+                <Code>"{node.value}"</Code>
+            </Box>
+        );
+    }
+    if (node.kind === 'BooleanValue') {
+        return (
+            <Box d="inline">
+                <Code>{node.value}</Code>
+            </Box>
+        );
+    }
+
+    if (node.kind === 'ListValue') {
+        return (
+            <Box d="inline">
+                {'['}
+                {node.values.map((t, idx) => {
+                    return (
+                        <Box>
+                            {`${idx ? ', ' : ''}`}
+                            <RootRenderer node={t} />
+                        </Box>
+                    );
+                })}
+                {']'}
+            </Box>
+        );
+    }
+    return <Box>UnknownNode: {node.kind}</Box>;
+}
+
+export const GraphUI = ({
+    schema
+}: {
+    schema: string
+}) => {
     const querysMap = getQuerysMap({ schema });
+    const initialQueryName = Object.keys(querysMap)[0];
+
+    // query name
+    const [queryName, setQueryName] = useState(initialQueryName);
+    // set current query string
+    const [query, setQuery] = useState(`query ${queryName} {
+    ${querysMap[initialQueryName]}
+}`)
+    // query to astnode
+    const [ast, setAst] = useState<DocumentNode | any>(null);
+    // fields
+    const [fields, setFields] = useState(getFields({ schema, name: queryName }));
+
     useEffect(() => {
-        // TODO: query を作成する関数をひとまとめにしてここで query にセットする
-        setQuery(initialQuery);
+        const _ast = parse(query);
+        setAst(_ast);
     }, []);
+
+    const handleChangeQueryName = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        const newQuery = `query ${value} {
+    ${querysMap[initialQueryName]}
+}`;
+
+        setQueryName(value);
+        setQuery(newQuery)
+        setAst(parse(newQuery));
+    }, [queryName, query]);
 
     return (
         <>
@@ -178,8 +326,28 @@ export const GraphUI = () => {
                 }}
             >
                 <Flex>
-                    takurinton
+                    <chakra.p fontSize="md">query name: </chakra.p>
+                    <Input size="md" placeholder='query name' value={queryName} onChange={(event) => handleChangeQueryName(event)} />
                 </Flex>
+                <Box>
+                    <Select
+                        defaultValue={fields}
+                        isMulti
+                        name="colors"
+                        options={fields}
+                        className="basic-multi-select"
+                        classNamePrefix="select"
+                    />
+                </Box>
+                <Box>
+                    {/* {ast === null ? <>loading</> : <RootRenderer node={ast} />} */}
+                    <Box pb={3} />
+                    <pre>
+                        <Code>
+                            {query}
+                        </Code>
+                    </pre>
+                </Box>
             </Provider>
         </>
     )
